d29347211403544ee58bc4f4ad70f533de84c25a
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-19 18:21:52 -0300

    Input & Button

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = styled(RectButton)`
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  height: 60px;
+
+  background-color: #ff9000;
+  border-radius: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  font-family: 'RobotoSlab-Medium';
+  font-size: 18px;
+  color: #312e38;
+`;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -4,6 +4,8 @@ export const Container = styled.View`
   flex: 1 0;
   align-items: center;
   justify-content: center;
+
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
