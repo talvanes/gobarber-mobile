1421e4c364d47b80dc9ec245b4016003f65b4126
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-20 10:54:30 -0300

    Tela de Autenticação

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1 0;
@@ -14,4 +15,37 @@ export const Title = styled.Text`
   font-family: 'RobotoSlab-Medium';
   font-size: 24px;
   color: #f4ede8;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+  background-color: #312e38;
+  border-color: #232129;
+  border-top-width: 1px;
+
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
