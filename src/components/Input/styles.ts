09d404b53cffc417138b331af2a56eda5923ec68
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-08-24 12:50:09 -0300

    Foco em Formulários

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,7 +1,11 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-export const Container = styled.View`
+interface ContainerProps {
+  isFocused: boolean;
+}
+
+export const Container = styled.View<ContainerProps>`
   width: 100%;
   height: 60px;
   padding: 0 16px;
@@ -9,6 +13,15 @@ export const Container = styled.View`
 
   border-radius: 10px;
   background-color: #232129;
+
+  border-width: 2px;
+  border-color: #232129;
+
+  ${props =>
+    props.isFocused &&
+    css`
+      border-color: #ff9000;
+    `}
 
   flex-direction: row;
   align-items: center;
